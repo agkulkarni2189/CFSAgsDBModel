22fb246b4237e1df1c7d437af1a69ceb6ffa674e
agkulkarni2189 <36198855+agkulkarni2189@users.noreply.github.com>
2018-10-12 17:59:48 +0530

    Project

---
+++ DIMSContainerDBEFDLL/DBUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIMSContainerDBEFDLL
+{
+    public class DBUtility
+    {
+        private DIMContainerDB_RevisedEntities dcre = null;
+
+        public DBUtility()
+        {
+            dcre = new DIMContainerDB_RevisedEntities();
+            ObjectContext context = ((IObjectContextAdapter)dcre).ObjectContext;
+
+            if (!context.DatabaseExists())
+                context.CreateDatabase();
+        }
+    }
+}
